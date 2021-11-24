57f42ef6d38c4d4b9f18043ccca8372e25d4efae
ibkarulloh <ibkaulloh21@gmail.com>
2021-11-24 15:16:59 +0700

    belajar

--- src/Chapter8/latihan/Array.java
+++ src/Chapter8/latihan/Array.java
@@ -1,19 +1,36 @@
 package Chapter8.latihan;
 
-import java.util.Arrays;
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        int[] a;
-        try (Scanner input = new Scanner(System.in)) {
-            a = new int[10];
-            System.out.println(Arrays.toString(a) + " ");
-            for (int i = 0; i < 10; i++) {
-                a[i] = input.nextInt();
+        Scanner input = new Scanner( System.in);
+        int[] nilai = new int[10];
+        int max,min;
+        int i;
+
+        for (i = 0; i<10; i++)
+        {
+            String bilangan = JOptionPane.showInputDialog("Masukkan Nilai: ");
+            nilai[i] = Integer.parseInt(bilangan);
+            System.out.println("Input Angka: "+bilangan);
+        }
+
+
+        min=nilai[0];
+        max=nilai[0];
+        for (i=0; i<10; i++){
+            if(nilai[i] > max){
+                max = nilai[i];
+            }
+            else if(nilai[i] < min){
+                min = nilai[i];
             }
         }
-        for (int j : a)
-            System.out.print(j + " ");
+        System.out.println("--------------------------------");
+        System.out.println("Nilai Max = "+max);
+        System.out.println("Nilai Min = "+min);
     }
 }
